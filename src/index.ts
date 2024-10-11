fff8b4e4155e4eb803a0e6166e99d683fc6f0680
gurgen22 <gurgenk022@gmail.com>
2024-10-12 00:29:29 +0300

    Added sendXLMToMultipleRecipients, sendXLM, and transactionHistory APIs.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
-import { Keypair, Server, Networks } from 'soroban-client';
+import { Keypair, Server, Networks, TransactionBuilder, Operation, Asset, Memo, Account } from 'soroban-client';
 import fetch from 'node-fetch';
+import { send } from 'process';
 
 const server = new Server('https://horizon-testnet.stellar.org');
 const networkPassphrase = Networks.TESTNET;
@@ -18,6 +19,14 @@ interface AccountData {
   sequence: string;
   balances: Balance[];
 }
+
+interface TransactionRecord {
+  id: string;
+  paging_token: string;
+  created_at: string;
+  source_account: string;
+}
+
 
 async function createWallet(): Promise<{ publicKey: string, secretKey: string }> {
   const keypair = Keypair.random();
@@ -66,6 +75,141 @@ async function getWalletBalance(publicKey: string): Promise<any> {
   }
 }
 
+async function getAccount(accountId: string): Promise<any> {
+  const url = `https://horizon-testnet.stellar.org/accounts/${accountId}`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching account information:', error);
+  }
+}
+
+async function sendXLM(senderSecretKey: string, recipientPublicKey: string, amount: string, message: string): Promise<void> {
+  try {
+    const senderKeypair = Keypair.fromSecret(senderSecretKey);
+    const publicKey = senderKeypair.publicKey();
+
+    const accountResponse = await getAccount(publicKey);
+
+    const account = new Account(accountResponse.id, accountResponse.sequence);
+
+    const transaction = new TransactionBuilder(account, {
+      fee: '100',
+      networkPassphrase,
+    })
+      .addOperation(Operation.payment({
+        destination: recipientPublicKey,
+        asset: Asset.native(),
+        amount: amount,
+      }))
+      .addMemo(Memo.text(message))
+      .setTimeout(30)
+      .build();
+    transaction.sign(senderKeypair);
+
+    const response = await fetch('https://horizon-testnet.stellar.org/transactions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({ tx: transaction.toXDR() }),
+    });
+
+    const responseData = await response.json();
+
+    if (response.ok) {
+      console.log('Transfer successful:', responseData);
+    } else {
+      console.error('Transfer error:', responseData);
+      throw new Error('Transfer failed');
+    }
+  } catch (error) {
+    console.error('Transfer error:', error);
+    throw new Error('Transfer failed');
+  }
+}
+
+async function sendXLMToMultipleRecipients(
+  senderSecretKey: string,
+  recipients: { recipientPublicKey: string; amount: string; message?: string }[],
+): Promise<void> {
+  try {
+    const senderKeypair = Keypair.fromSecret(senderSecretKey);
+    console.log('senderKeypair publicKey', senderKeypair.publicKey());
+    const publicKey = senderKeypair.publicKey();
+
+    const accountResponse = await getAccount(publicKey);
+    const account = new Account(accountResponse.id, accountResponse.sequence);
+
+    const transactionBuilder = new TransactionBuilder(account, {
+      fee: '100',
+      networkPassphrase,
+    });
+
+    for (const recipient of recipients) {
+      const amountString = recipient.amount.toString();
+      transactionBuilder.addOperation(Operation.payment({
+        destination: recipient.recipientPublicKey,
+        asset: Asset.native(),
+        amount: amountString,
+      }));
+
+      if (recipient.message) {
+        transactionBuilder.addMemo(Memo.text(recipient.message));
+      }
+    }
+
+    const transaction = transactionBuilder.setTimeout(30).build();
+    transaction.sign(senderKeypair);
+
+    const response = await fetch('https://horizon-testnet.stellar.org/transactions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({ tx: transaction.toXDR() }),
+    });
+
+    const responseData = await response.json();
+
+    if (response.ok) {
+      console.log('Transfer successful:', responseData);
+    } else {
+      console.error('Transfer error:', responseData);
+      throw new Error('Transfer failed');
+    }
+  } catch (error) {
+    console.error('Transfer error:', error);
+    throw new Error('Transfer failed');
+  }
+}
+
+async function getTransactionHistory(publicKey: string): Promise<TransactionRecord[]> {
+  try {
+    const response = await fetch(`https://horizon-testnet.stellar.org/accounts/${publicKey}/transactions`);
+    if (!response.ok) {
+      throw new Error('Transaction history could not be retrieved.');
+    }
+    const transactionData = await response.json() as { _embedded: { records: TransactionRecord[] } };
+    return transactionData._embedded.records;
+  } catch (error) {
+    console.error('An error occurred while retrieving transaction history:', error);
+    return [];
+  }
+}
+
+
+
+
+
+
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -109,4 +253,45 @@ app.get('/getBalance', async (req: Request, res: Response) => {
     res.status(500).send({ message: 'An error occurred while checking the balance' });
   }
 });
+
+app.post('/sendXLM', async (req: Request, res: Response) => {
+  try {
+    const { senderSecretKey, recipientPublicKey, message } = req.body;
+    let amount = req.body.amount;
+    amount = amount.toString();
+    const response = await sendXLM(senderSecretKey, recipientPublicKey, amount, message);
+    res.status(200).send({ message: 'Transfer successful', response });
+  } catch (error) {
+    res.status(500).send({ message: 'Transfer failed', error });
+  }
+});
+interface Recipient {
+  recipientPublicKey: string;
+  amount: string;
+  message?: string;
+}
+
+app.post('/sendXLMToMultipleRecipients', async (req: Request, res: Response) => {
+  try {
+    const { senderSecretKey, recipients }: { senderSecretKey: string; recipients: Recipient[] } = req.body;
+    const response = await sendXLMToMultipleRecipients(senderSecretKey, recipients);
+
+    res.status(200).send({ message: 'Transfers successful', response });
+  } catch (error) {
+    res.status(500).send({ message: 'Error sendXLMToMultipleRecipients:', error });
+  }
+});
+
+
+app.get('/transactionHistory', async (req: Request, res: Response) => {
+  try {
+    const { publicKey } = req.body;
+    const transactions = await getTransactionHistory(publicKey);
+    res.status(200).send({ transactions });
+  } catch (error) {
+    console.error('An error occurred while fetching transaction history:', error);
+    res.status(500).send({ message: 'An error occurred while fetching transaction history.' });
+  }
+});
+
 
